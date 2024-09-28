8d569ae2c8e6eff02f67e35855c9a23a9fd30cd6
samheart564 <samheart564@gmail.com>
2024-09-28 12:26:19 -0700

    moved navbar constants outside the component itself

--- src/components/_common/Navbar/Navbar.tsx
+++ src/components/_common/Navbar/Navbar.tsx
@@ -1,6 +1,57 @@
 import React, { useState } from "react"
 import "./styles.css"
 
+// Define all the pages and their structure in a constant array
+const navbarPages = [
+  { name: "Newbie Tips", href: "newbie_tips", icon: "fa-lightbulb" },
+  {
+    name: "Gameplay",
+    icon: "fa-ship",
+    isDropdown: true,
+    dropdownItems: [
+      {
+        name: "Early Ship Recommendations",
+        href: "early_ship_recommendations",
+      },
+      { name: "Equipment", href: "equipment" },
+      { name: "Farming", href: "farming" },
+      { name: "Fleetbuilding", href: "fleetbuilding" },
+      { name: "Fleet Technology", href: "fleet_technology" },
+      { name: "Research", href: "research" },
+      { name: "Samvaluations", href: "samvaluation" },
+    ],
+  },
+  {
+    name: "Resource",
+    icon: "fa-gem",
+    isDropdown: true,
+    dropdownItems: [
+      { name: "Common Resources", href: "common_resource" },
+      { name: "Shop Priority", href: "shop_priority" },
+    ],
+  },
+  { name: "Tools", href: "tools", icon: "fa-wrench" },
+  {
+    name: "Changelog",
+    href: "changelog",
+    icon: "fa-clock-rotate-left",
+    hiddenOnLarge: true,
+  },
+  {
+    name: "Contributors",
+    href: "contributors",
+    icon: "fa-user-pen",
+    hiddenOnLarge: true,
+  },
+  {
+    name: "Discord",
+    href: "https://discord.gg/wKJKxq5WQt",
+    icon: "fa-brands fa-discord",
+    external: true,
+    hiddenOnLarge: true,
+  },
+]
+
 interface NavbarProps {
   activePage?: string | null
 }
@@ -16,57 +67,6 @@ export const Navbar: React.FC<NavbarProps> = ({ activePage }) => {
   const toggleDropdown = (dropdown: string) => {
     setActiveDropdown(activeDropdown === dropdown ? "" : dropdown)
   }
-
-  // Define all the pages and their structure in a constant array
-  const navbarPages = [
-    { name: "Newbie Tips", href: "newbie_tips", icon: "fa-lightbulb" },
-    {
-      name: "Gameplay",
-      icon: "fa-ship",
-      isDropdown: true,
-      dropdownItems: [
-        {
-          name: "Early Ship Recommendations",
-          href: "early_ship_recommendations",
-        },
-        { name: "Equipment", href: "equipment" },
-        { name: "Farming", href: "farming" },
-        { name: "Fleetbuilding", href: "fleetbuilding" },
-        { name: "Fleet Technology", href: "fleet_technology" },
-        { name: "Research", href: "research" },
-        { name: "Samvaluations", href: "samvaluation" },
-      ],
-    },
-    {
-      name: "Resource",
-      icon: "fa-gem",
-      isDropdown: true,
-      dropdownItems: [
-        { name: "Common Resources", href: "common_resource" },
-        { name: "Shop Priority", href: "shop_priority" },
-      ],
-    },
-    { name: "Tools", href: "tools", icon: "fa-wrench" },
-    {
-      name: "Changelog",
-      href: "changelog",
-      icon: "fa-clock-rotate-left",
-      hiddenOnLarge: true,
-    },
-    {
-      name: "Contributors",
-      href: "contributors",
-      icon: "fa-user-pen",
-      hiddenOnLarge: true,
-    },
-    {
-      name: "Discord",
-      href: "https://discord.gg/wKJKxq5WQt",
-      icon: "fa-brands fa-discord",
-      external: true,
-      hiddenOnLarge: true,
-    },
-  ]
 
   return (
     <nav
