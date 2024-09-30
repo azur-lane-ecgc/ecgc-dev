e7966c9b97e12403c3d795311482fdcea8cd5d59
samheart564 <samheart564@gmail.com>
2024-09-29 23:52:18 -0700

    changed container sizes to be the same as bootstrap

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,34 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+const tailwindConfig = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  corePlugins: {
+    container: false,
+  },
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        ".container": {
+          width: "100%",
+          maxWidth: "100%",
+          "@screen sm": {
+            maxWidth: "540px",
+          },
+          "@screen md": {
+            maxWidth: "720px",
+          },
+          "@screen lg": {
+            maxWidth: "960px",
+          },
+          "@screen xl": {
+            maxWidth: "1140px",
+          },
+        },
+      })
+    },
+  ],
 }
+
+export default tailwindConfig
