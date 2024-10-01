25e7a845d4159fc615344a709a00bc27f2b6bf2d
samheart564 <samheart564@gmail.com>
2024-09-30 18:12:57 -0700

    fixed deployment stuff yay

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@
 import { defineConfig } from "astro/config"
 
 import react from "@astrojs/react"
-
-import tailwind from "@astrojs/tailwind";
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://samheart564.github.io",
+  site: "https://samheart564.github.io/test_ecgc_2/",
   integrations: [react(), tailwind()],
-  base: "",
+  base: "/test_ecgc_2/",
+  output: "static",
 })
