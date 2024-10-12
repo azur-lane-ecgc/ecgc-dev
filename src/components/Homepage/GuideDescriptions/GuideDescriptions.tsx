866cd24ea8dbae28c53acba0979e191bd7d93cb7
samheart564 <samheart564@gmail.com>
2024-10-12 13:36:28 -0700

    added mobile guide descriptions with icons only

--- src/components/Homepage/GuideDescriptions/GuideDescriptions.tsx
+++ src/components/Homepage/GuideDescriptions/GuideDescriptions.tsx
@@ -1,5 +1,7 @@
 import React from "react"
 
+import "./styles.css"
+
 interface Guide {
   title: string
   author: string
@@ -26,7 +28,7 @@ const guides: Guide[] = [
     image: "images/misc/common_resource_cover.jpg",
   },
   {
-    title: "Early Ship Recommendations Guide",
+    title: "Early Ship Recommendations",
     author: "Samheart564",
     date: "11/24/2023",
     revision: "1.1.4",
@@ -164,51 +166,90 @@ const guides: Guide[] = [
 
 export const GuideDescriptions: React.FC = () => {
   return (
-    <div id="images" className="hidden sm:block">
-      <div className="flex flex-wrap -mx-3">
-        {guides.map((guide, index) => (
-          <div
-            key={index}
-            className="w-full px-3 mt-3 sm:mt-0 text-left gridcell-list-style"
-          >
-            <div className="flex mt-1">
-              <div className="flex-shrink-0 w-[80px]">
-                <a href={guide.link} target="_self" rel="noopener noreferrer">
-                  <img
-                    className="h-[100px] rounded shadow-lg object-cover"
-                    src={guide.image}
-                    alt={`${guide.title} cover`}
-                  />
-                </a>
+    <>
+      {/* Images w/ Description */}
+      <div id="images" className="hidden md:block">
+        <div className="flex flex-wrap -mx-3">
+          {guides.map((guide, index) => (
+            <div
+              key={index}
+              className="w-full px-3 mt-3 sm:mt-0 text-left gridcell-list-style"
+            >
+              <div className="flex mt-1">
+                <div className="flex-shrink-0 w-[80px]">
+                  <a href={guide.link} target="_self" rel="noopener noreferrer">
+                    <img
+                      className="h-[100px] rounded shadow-lg object-cover"
+                      src={guide.image}
+                      alt={`${guide.title} cover`}
+                    />
+                  </a>
+                </div>
+                <div className="ml-4 flex-grow mt-1">
+                  <a href={guide.link} target="_self" rel="noopener noreferrer">
+                    <div className="text-sm font-bold uppercase mb-4 guide-title">
+                      {guide.title}
+                    </div>
+                  </a>
+                  <div className="text-xs mb-4 text-gray-600 mod-gridcell-details-style">
+                    <div>
+                      <i className="fas fa-pencil-alt mr-1" />
+                      &#8201;{guide.author}
+                    </div>
+                    <div>
+                      <i className="fas fa-sync mr-1" />
+                      &#8201;{guide.date}
+                    </div>
+                    <div>Revision {guide.revision}</div>
+                  </div>
+                </div>
               </div>
-              <div className="ml-4 flex-grow mt-1">
-                <a href={guide.link} target="_self" rel="noopener noreferrer">
-                  <div className="text-sm font-bold uppercase mb-4 guide-title">
-                    {guide.title}
-                  </div>
-                </a>
-                <div className="text-xs mb-4 text-gray-600 mod-gridcell-details-style">
-                  <div>
-                    <i className="fas fa-pencil-alt mr-1" />
-                    &#8201;{guide.author}
-                  </div>
-                  <div>
-                    <i className="fas fa-sync mr-1" />
-                    &#8201;{guide.date}
-                  </div>
-                  <div>Revision {guide.revision}</div>
+              <div className="mt-2">
+                <div className="guide-text">
+                  <br />
+                  <p>{guide.description}</p>
                 </div>
               </div>
             </div>
-            <div className="mt-2">
-              <div className="guide-text">
-                <br />
-                <p>{guide.description}</p>
+          ))}
+        </div>
+      </div>
+
+      {/* Images Only */}
+      <div className="block md:hidden" id="images">
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+          {guides.map((guide, index) => (
+            <div key={index} className="mt-5 text-center">
+              <a
+                href={guide.link}
+                target="_self"
+                rel="noopener noreferrer"
+                className="block"
+              >
+                <img
+                  className="w-[150px] h-[200px] mx-auto rounded shadow-lg object-cover"
+                  src={guide.image}
+                  alt={guide.title}
+                />
+                <div className="gridcell-title guide-title text-[95%] uppercase font-bold mt-2 mb-4">
+                  {guide.title}
+                </div>
+              </a>
+              <div className="gridcell-details-style-2 text-[80%] mb-4">
+                <div>
+                  <i className="fas fa-pencil-alt mr-1" />
+                  {guide.author}
+                </div>
+                <div>
+                  <i className="fas fa-sync mr-1" />
+                  {guide.date}
+                </div>
+                <div>Revision {guide.revision}</div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   )
 }
