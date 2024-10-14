d059cb329e457d98916367d5c077918dbccc0958
samheart564 <samheart564@gmail.com>
2024-10-13 22:32:41 -0700

    fixed hyperlinks between pages yay

--- src/components/_common/Navbar/Navbar.tsx
+++ src/components/_common/Navbar/Navbar.tsx
@@ -119,7 +119,7 @@ const NavItem: React.FC<NavItemProps> = ({ page, activePage }) => {
               {page.dropdownItems?.map((item, idx) => (
                 <a
                   key={idx}
-                  href={item.href}
+                  href={`./${item.href}`}
                   className={`block px-3 py-1 text-[1rem]
                     ${
                       activePage === item.href
@@ -140,7 +140,7 @@ const NavItem: React.FC<NavItemProps> = ({ page, activePage }) => {
 
   return (
     <a
-      href={page.href}
+      href={`./${page.href}`}
       className={`navbar-link flex items-center px-2 py-2 rounded-md text-base font-medium min-h-[40px]
         ${
           activePage === page.href ? "navbar-active" : ""
@@ -196,7 +196,7 @@ const MobileNavItem: React.FC<MobileNavItemProps> = ({
             {page.dropdownItems?.map((item, idx) => (
               <a
                 key={idx}
-                href={item.href}
+                href={`./${item.href}`}
                 className={`block px-3 py-2 rounded-md text-base font-normal
                   ${
                     activePage === item.href
@@ -215,7 +215,7 @@ const MobileNavItem: React.FC<MobileNavItemProps> = ({
 
   return (
     <a
-      href={page.href}
+      href={`./${page.href}`}
       className={`navbar-link flex items-center px-3 py-2 rounded-md text-base font-medium no-underline
         ${
           activePage === page.href ? "navbar-active" : ""
