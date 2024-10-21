f26332445bb414f56f2ab3a857a24eaa36136d0d
samheart564 <samheart564@gmail.com>
2024-10-20 17:39:44 -0700

    moved guide description array to pageInfo since it can be used elsewhere too

--- src/components/Homepage/GuideDescriptions/GuideDescriptions.tsx
+++ src/components/Homepage/GuideDescriptions/GuideDescriptions.tsx
@@ -1,170 +1,12 @@
 import React from "react"
 
+import { pageInfo } from "@components/_common/PageInfo"
+
 import "./styles.css"
 
-interface Guide {
-  title: string
-  author: string
-  date: string
-  revision: string
-  description: React.ReactNode
-  link: string
-  image: string
-}
-
-const guides: Guide[] = [
-  {
-    title: "Common Resource Guide",
-    author: "Samheart564",
-    date: "6/9/2024",
-    revision: "1.1.5",
-    description: (
-      <>
-        Goes over Common Resources in Azur Lane, how to acquire them, and how
-        much you can acquire in a reasonable time frame.
-      </>
-    ),
-    link: "common_resource",
-    image: "/test_ecgc_2/images/misc/common_resource_cover.jpg",
-  },
-  {
-    title: "Early Ship Recommendations",
-    author: "Samheart564",
-    date: "11/24/2023",
-    revision: "1.1.4",
-    description: (
-      <>
-        Goes over easily accessible good ships in Azur Lane and where to acquire
-        them.
-      </>
-    ),
-    link: "early_ship_recommendations",
-    image: "/test_ecgc_2/images/misc/early_ship_recs_cover.png",
-  },
-  {
-    title: "Equipment Guide",
-    author: "Samheart564",
-    date: "9/5/2024",
-    revision: "1.15.0",
-    description: (
-      <>
-        Goes over the optimal equipment to use in Azur Lane, in an easy-to-read
-        image format!
-      </>
-    ),
-    link: "equipment",
-    image: "/test_ecgc_2/images/misc/equipment_guide_cover.png",
-  },
-  {
-    title: "Farming Guide",
-    author: "Samheart564",
-    date: "9/5/2024",
-    revision: "1.1.1",
-    description: <>Goes over the optimal ways to farm in Azur Lane.</>,
-    link: "farming",
-    image: "/test_ecgc_2/images/misc/farming_guide_cover.jpg",
-  },
-  {
-    title: "Fleetbuilding Guide",
-    author: "Samheart564",
-    date: "9/5/2024",
-    revision: "1.12.0",
-    description: (
-      <>
-        Goes over how to make fleets for both Campaign and{" "}
-        <a
-          href="https://azurlane.koumakan.jp/wiki/Operation Siren"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Operation Siren
-        </a>{" "}
-        in Azur Lane.
-      </>
-    ),
-    link: "fleetbuilding",
-    image: "/test_ecgc_2/images/misc/fleetbuilding_guide_cover.png",
-  },
-  {
-    title: "Fleet Technology Guide",
-    author: "Samheart564",
-    date: "9/5/2024",
-    revision: "1.1.1",
-    description: (
-      <>
-        Goes over the{" "}
-        <a
-          href="https://azurlane.koumakan.jp/wiki/Fleet_Technology"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Fleet Technology
-        </a>{" "}
-        mechanics and how to get Tech Points in Azur Lane.
-      </>
-    ),
-    link: "fleet_technology",
-    image: "/test_ecgc_2/images/misc/no_cover.jpg",
-  },
-  {
-    title: "Newbie Tips",
-    author: "Riceist, Samheart564",
-    date: "9/5/2024",
-    revision: "1.1.4",
-    description: (
-      <>Goes over basic advice and mistakes to avoid for new players!</>
-    ),
-    link: "newbie_tips",
-    image: "/test_ecgc_2/images/misc/no_cover.jpg",
-  },
-  {
-    title: "Research Guide",
-    author: "Samheart564",
-    date: "9/5/2024",
-    revision: "1.5.1",
-    description: (
-      <>
-        Goes over the{" "}
-        <a
-          href="https://azurlane.koumakan.jp/wiki/Research"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Research
-        </a>{" "}
-        mechanics in Azur Lane, including optimal Research Projects to take, and
-        how Catchup / Coin-up work.
-      </>
-    ),
-    link: "research",
-    image: "/test_ecgc_2/images/misc/research_guide_cover.jpg",
-  },
-  {
-    title: "Samvaluations",
-    author: "Samheart564",
-    date: "9/5/2024",
-    revision: "1.12.0",
-    description: <>Compiled list of all my ship reviews!</>,
-    link: "samvaluation",
-    image: "/test_ecgc_2/images/misc/no_cover.jpg",
-  },
-  {
-    title: "Shop Priority Guide",
-    author: "Riceist, Samheart564",
-    date: "9/5/2024",
-    revision: "1.7.1",
-    description: (
-      <>
-        Goes over all the permanent shops in Azur Lane, and what to obtain from
-        them.
-      </>
-    ),
-    link: "shop_priority",
-    image: "/test_ecgc_2/images/misc/no_cover.jpg",
-  },
-]
-
 export const GuideDescriptions: React.FC = () => {
+  const guides = pageInfo.filter((guide) => guide.title)
+
   return (
     <>
       {/* Images w/ Description */}
@@ -194,11 +36,20 @@ export const GuideDescriptions: React.FC = () => {
                   <div className="text-xs mb-4 text-gray-600 mod-gridcell-details-style">
                     <div>
                       <i className="fas fa-pencil-alt mr-1" />
-                      &#8201;{guide.author}
+                      &#8201;
+                      {guide.authors.length > 0
+                        ? guide.authors.map((author, index) => (
+                            <span key={index}>
+                              {author}
+                              {index < guide.authors.length - 1 ? ", " : ""}
+                            </span>
+                          ))
+                        : null}
                     </div>
+
                     <div>
                       <i className="fas fa-sync mr-1" />
-                      &#8201;{guide.date}
+                      &#8201;{guide.lastUpdated}
                     </div>
                     <div>Revision {guide.revision}</div>
                   </div>
@@ -238,11 +89,18 @@ export const GuideDescriptions: React.FC = () => {
               <div className="gridcell-details-style-2 text-[80%] mb-4">
                 <div>
                   <i className="fas fa-pencil-alt mr-1" />
-                  {guide.author}
+                  {guide.authors.length > 0
+                    ? guide.authors.map((author, index) => (
+                        <span key={index}>
+                          {author}
+                          {index < guide.authors.length - 1 ? ", " : ""}
+                        </span>
+                      ))
+                    : null}
                 </div>
                 <div>
                   <i className="fas fa-sync mr-1" />
-                  {guide.date}
+                  {guide.lastUpdated}
                 </div>
                 <div>Revision {guide.revision}</div>
               </div>
---
+++ src/components/_common/PageInfo/pageInfo.tsx
@@ -0,0 +1,161 @@
+interface PageInfoItem {
+  authors: string[] //array of authors
+  lastUpdated: string
+  title: string
+  revision: string
+  description: JSX.Element
+  link: string
+  image: string
+}
+
+export const pageInfo: PageInfoItem[] = [
+  {
+    title: "Common Resource Guide",
+    authors: ["Samheart564"],
+    lastUpdated: "6/9/2024",
+    revision: "1.1.5",
+    description: (
+      <>
+        Goes over Common Resources in Azur Lane, how to acquire them, and how
+        much you can acquire in a reasonable time frame.
+      </>
+    ),
+    link: "common_resource",
+    image: "/test_ecgc_2/images/misc/common_resource_cover.jpg",
+  },
+  {
+    title: "Early Ship Recommendations",
+    authors: ["Samheart564"],
+    lastUpdated: "11/24/2023",
+    revision: "1.1.4",
+    description: (
+      <>
+        Goes over easily accessible good ships in Azur Lane and where to acquire
+        them.
+      </>
+    ),
+    link: "early_ship_recommendations",
+    image: "/test_ecgc_2/images/misc/early_ship_recs_cover.png",
+  },
+  {
+    title: "Equipment Guide",
+    authors: ["Samheart564"],
+    lastUpdated: "9/5/2024",
+    revision: "1.15.0",
+    description: (
+      <>
+        Goes over the optimal equipment to use in Azur Lane, in an easy-to-read
+        image format!
+      </>
+    ),
+    link: "equipment",
+    image: "/test_ecgc_2/images/misc/equipment_guide_cover.png",
+  },
+  {
+    title: "Farming Guide",
+    authors: ["Samheart564"],
+    lastUpdated: "9/5/2024",
+    revision: "1.1.1",
+    description: <>Goes over the optimal ways to farm in Azur Lane.</>,
+    link: "farming",
+    image: "/test_ecgc_2/images/misc/farming_guide_cover.jpg",
+  },
+  {
+    title: "Fleetbuilding Guide",
+    authors: ["Samheart564"],
+    lastUpdated: "9/5/2024",
+    revision: "1.12.0",
+    description: (
+      <>
+        Goes over how to make fleets for both Campaign and{" "}
+        <a
+          href="https://azurlane.koumakan.jp/wiki/Operation Siren"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Operation Siren
+        </a>{" "}
+        in Azur Lane.
+      </>
+    ),
+    link: "fleetbuilding",
+    image: "/test_ecgc_2/images/misc/fleetbuilding_guide_cover.png",
+  },
+  {
+    title: "Fleet Technology Guide",
+    authors: ["Samheart564"],
+    lastUpdated: "9/5/2024",
+    revision: "1.1.1",
+    description: (
+      <>
+        Goes over the{" "}
+        <a
+          href="https://azurlane.koumakan.jp/wiki/Fleet_Technology"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Fleet Technology
+        </a>{" "}
+        mechanics and how to get Tech Points in Azur Lane.
+      </>
+    ),
+    link: "fleet_technology",
+    image: "/test_ecgc_2/images/misc/no_cover.jpg",
+  },
+  {
+    title: "Newbie Tips",
+    authors: ["Riceist", "Samheart564"],
+    lastUpdated: "9/5/2024",
+    revision: "1.1.4",
+    description: (
+      <>Goes over basic advice and mistakes to avoid for new players!</>
+    ),
+    link: "newbie_tips",
+    image: "/test_ecgc_2/images/misc/no_cover.jpg",
+  },
+  {
+    title: "Research Guide",
+    authors: ["Samheart564"],
+    lastUpdated: "9/5/2024",
+    revision: "1.5.1",
+    description: (
+      <>
+        Goes over the{" "}
+        <a
+          href="https://azurlane.koumakan.jp/wiki/Research"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Research
+        </a>{" "}
+        mechanics in Azur Lane, including optimal Research Projects to take, and
+        how Catchup / Coin-up work.
+      </>
+    ),
+    link: "research",
+    image: "/test_ecgc_2/images/misc/research_guide_cover.jpg",
+  },
+  {
+    title: "Samvaluations",
+    authors: ["Samheart564"],
+    lastUpdated: "9/5/2024",
+    revision: "1.12.0",
+    description: <>Compiled list of all my ship reviews!</>,
+    link: "samvaluation",
+    image: "/test_ecgc_2/images/misc/no_cover.jpg",
+  },
+  {
+    title: "Shop Priority Guide",
+    authors: ["Riceist", "Samheart564"],
+    lastUpdated: "9/5/2024",
+    revision: "1.7.1",
+    description: (
+      <>
+        Goes over all the permanent shops in Azur Lane, and what to obtain from
+        them.
+      </>
+    ),
+    link: "shop_priority",
+    image: "/test_ecgc_2/images/misc/no_cover.jpg",
+  },
+]
