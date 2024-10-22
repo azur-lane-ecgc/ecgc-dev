f4b8e9416b10a306aac15a7e904029b365440d96
samheart564 <samheart564@gmail.com>
2024-10-21 18:15:39 -0700

    added newbietips toc

--- dev_tools/_pageInfo.js
+++ dev_tools/_pageInfo.js
@@ -7,4 +7,8 @@ export const pageInfo = [
     name: "Changelog2023",
     path: "src/components/Changelog/Changelog2023Content.astro",
   },
+  {
+    name: "Newbie Tips",
+    path: "src/components/NewbieTips/NewbieTipsContent.astro",
+  },
 ]
