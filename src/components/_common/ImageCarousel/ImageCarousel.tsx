604b5f71964486695615b31471fe43b6a6d13e31
samheart564 <samheart564@gmail.com>
2024-11-16 23:41:03 -0800

    finished image carousel component

--- src/components/_common/ImageCarousel/ImageCarousel.tsx
+++ src/components/_common/ImageCarousel/ImageCarousel.tsx
@@ -16,6 +16,8 @@ export const ImageCarousel: React.FC<ImageCarouselProps> = ({ data }) => {
   const isOneElement = data.length <= 1
   const [slide, setSlide] = useState(0)
   const [play, setPlay] = useState(true)
+  const [caption, setCaption] = useState(true)
+  const [userInteract, setuserInteract] = useState(false)
 
   const slideChange = (newIndex: number) => {
     setSlide((slide + newIndex + data.length) % data.length)
@@ -32,22 +34,55 @@ export const ImageCarousel: React.FC<ImageCarouselProps> = ({ data }) => {
     }
   }, [play, slide])
 
+  // automatically disable / enable caption at small screen size transition
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 540 && caption && !userInteract) {
+        setCaption(false)
+      }
+      if (window.innerWidth >= 540 && !caption && !userInteract) {
+        setCaption(true)
+      }
+    }
+    handleResize()
+    window.addEventListener("resize", handleResize)
+
+    return () => {
+      window.removeEventListener("resize", handleResize)
+    }
+  }, [caption])
+
   return (
     <div className="carousel">
       <div className="carousel-inner">
         {data.map((item, index) => (
-          <img
-            className={`slide ${slide === index ? "active" : ""}`}
-            key={index}
-            src={item.src}
-            alt={item.alt || item.caption}
-          />
+          <div className={`slide ${slide === index ? "active" : ""}`}>
+            <img
+              className="carousel-image"
+              key={index}
+              src={item.src}
+              alt={item.alt || item.caption}
+            />
+            {caption && (
+              <div className="carousel-caption">
+                <p>{item.caption}</p>
+              </div>
+            )}
+          </div>
         ))}
       </div>
+
+      <i
+        className={`toggle cc fa-solid ${caption ? "fa-eye" : "fa-eye-slash"}`}
+        onClick={() => {
+          setCaption((prev) => !prev)
+          setuserInteract(true)
+        }}
+      />
       {!isOneElement && (
         <>
           <i
-            className={`cycle-toggle fa-solid ${play ? "fa-pause" : "fa-play"}`}
+            className={`toggle cycle fa-solid ${play ? "fa-pause" : "fa-play"}`}
             onClick={() => setPlay((prev) => !prev)}
           />
           <i
