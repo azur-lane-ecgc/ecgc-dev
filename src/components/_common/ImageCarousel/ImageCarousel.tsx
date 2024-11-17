27e7415dea56ef459907dbb818caec8b6a3388d5
samheart564 <samheart564@gmail.com>
2024-11-16 21:38:48 -0800

    very close to working carousel

--- src/components/_common/ImageCarousel/ImageCarousel.tsx
+++ src/components/_common/ImageCarousel/ImageCarousel.tsx
@@ -22,44 +22,52 @@ export const ImageCarousel: React.FC<ImageCarouselProps> = ({ data }) => {
   }
 
   return (
-    <div className="carousel">
-      <div className="carousel-inner">
-        {data.map((item, index) => (
-          <img
-            className={`slide ${slide === index ? "active" : ""}`}
-            src={item.src}
-            alt={item.alt || item.caption}
-            key={index}
-          />
-        ))}
+    <div className="container">
+      <div className="carousel">
+        <div className="carousel-inner">
+          {data.map((item, index) => (
+            <div
+              className={`slide ${slide === index ? "active" : ""}`}
+              key={index}
+            >
+              <img
+                className="carousel-image"
+                src={item.src}
+                alt={item.alt || item.caption}
+              />
+            </div>
+          ))}
+        </div>
+        {!isOneElement && (
+          <>
+            <i
+              className={`cycleToggle fa-solid ${play ? "fa-play" : "fa-pause"}`}
+              onClick={() => setPlay((prev) => !prev)}
+            />
+            <i
+              className="fa-solid fa-chevron-left arrow"
+              onClick={() => slideChange(-1)}
+            />
+            <i
+              className="fa-solid fa-chevron-right arrow"
+              onClick={() => slideChange(1)}
+            />
+            <span className="indicators">
+              {data.map((_, index) => {
+                return (
+                  <button
+                    key={index}
+                    onClick={() => setSlide(index)}
+                    className={
+                      slide === index ? "indicator active" : "indicator"
+                    }
+                  ></button>
+                )
+              })}
+            </span>
+          </>
+        )}
       </div>
-      {!isOneElement && (
-        <>
-          <i
-            className={`cycleToggle fa-solid ${play ? "fa-play" : "fa-pause"}`}
-            onClick={() => setPlay((prev) => !prev)}
-          />
-          <i
-            className="fa-solid fa-chevron-left arrow"
-            onClick={() => slideChange(-1)}
-          />
-          <i
-            className="fa-solid fa-chevron-right arrow"
-            onClick={() => slideChange(1)}
-          />
-          <span className="indicators">
-            {data.map((_, index) => {
-              return (
-                <button
-                  key={index}
-                  onClick={() => setSlide(index)}
-                  className={slide === index ? "indicator active" : "indicator"}
-                ></button>
-              )
-            })}
-          </span>
-        </>
-      )}
     </div>
   )
 }
