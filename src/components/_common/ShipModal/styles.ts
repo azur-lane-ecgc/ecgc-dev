b1fe6563b753e04c967c2ac41fe2f769fadd789c
samheart564 <samheart564@gmail.com>
2024-12-15 23:46:51 -0800

    rearranged styles slightly

--- src/components/_common/ShipModal/styles.ts
+++ src/components/_common/ShipModal/styles.ts
@@ -3,17 +3,16 @@ export const modalTriggerStyle =
 
 export const modalOverlayStyle =
   "h-[100%] w-[100%] fixed inset-0 bg-black bg-opacity-40 z-[99] overflow-y-auto"
-
 export const modalStyle =
   "bg-ecgc-secondary rounded-lg shadow-lg w-full md:min-w-[750px] md:w-7/12 p-7 relative my-[1.75rem] mx-auto"
 
 export const closeButtonStyle =
-  "absolute top-2 right-2 text-gray-500 hover:text-gray-300 text-xl"
+  "absolute top-2 right-2 text-gray-500 hover:text-gray-300 text-3xl"
+
+export const shipLinkStyle =
+  "text-white bg-transparent no-underline visited:text-white visited:bg-transparent hover:text-[hsla(0,0%,100%,0.75)] hover:bg-transparent hover:underline active:text-red-500 active:bg-transparent active:underline"
 
 export const shipIconContainerStyle =
   "flex flex-col sm:flex-row justify-center items-center gap-4 mt-5"
 export const shipIconStyle =
   "overflow-hidden my-1.5 min-w-fit w-fit p-0.5 shadow-[0_10px_25px_0_rgba(0,0,0,1)] h-auto border-radius-0"
-
-export const shipLinkStyle =
-  "text-white bg-transparent no-underline visited:text-white visited:bg-transparent hover:text-[hsla(0,0%,100%,0.75)] hover:bg-transparent hover:underline active:text-red-500 active:bg-transparent active:underline"
