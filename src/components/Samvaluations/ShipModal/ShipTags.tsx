db63f8259c5ef1731747868cd40f24aacfd232ff
samheart564 <samheart564@gmail.com>
2024-12-17 19:54:48 -0800

    finalized ship tag section

--- src/components/Samvaluations/ShipModal/ShipTags.tsx
+++ src/components/Samvaluations/ShipModal/ShipTags.tsx
@@ -16,62 +16,64 @@ export const ShipTags: React.FC<ShipTags> = ({ hullType, faction, roles }) => {
       {/* Small Screen Tag Trigger */}
       <button
         onClick={() => setRoleDropdown(!roleDropdownOpen)}
-        className={`absolute top-2 left-2 text-base flex items-center space-x-2 ${roleDropdownOpen ? "text-green-600" : "text-cyan-400"} hover:text-green-600 md:hidden`}
+        className={`bg-[#3b444bb9] absolute top-0 left-0 p-[4px] rounded-lg text-base flex items-center space-x-2 border-2 border-gray-400 text-gray-300 ${roleDropdownOpen ? "border-pink-300 text-amber-500" : ""} hover:text-cyan-300 hover:border-amber-500 md:hidden`}
       >
-        <span>Tags</span>
         <i
           className={`fa-solid translate-y-[1px] ${roleDropdownOpen ? "fa-angle-up" : "fa-angle-down"}`}
         />
       </button>
       {/* Tags (Smaller Screen) */}
-      {roleDropdownOpen && (
-        <div className="bg-ecgc-secondary absolute md:hidden text-center top-[40px] left-0 w-[160px] border-l border-r border-t border-b-transparent border-gray-500 shadow-lg overflow-y-auto max-h-[200px] z-50">
-          {/* Faction Icon */}
-          <div className="w-full flex justify-between border-b border-gray-600 py-1 px-5">
-            <span className="w-[40px] h-[40px] overflow-hidden relative inline-block">
-              <img
-                loading="lazy"
-                src={`/test_ecgc_2/images/faction/${faction}.png`}
-                alt={`${faction}`}
-                className="absolute top-0 left-0 w-full h-auto"
-              />{" "}
-            </span>
-            <span className="flex items-center justify-center fake-modal-link">
-              {faction}
-            </span>
-          </div>
 
-          {/* HullType Icon */}
-          <div className="w-full flex justify-between border-b border-gray-600 py-1 px-5">
-            <span className="w-[40px] h-[40px] flex items-center justify-center overflow-hidden relative">
-              <img
-                loading="lazy"
-                src={`/test_ecgc_2/images/ship_type/${hullType}.png`}
-                alt={`${hullType}`}
-                className="w-full h-full object-contain"
-              />
-            </span>
-            <span className="flex items-center justify-center fake-modal-link">
-              {hullType}
-            </span>
-          </div>
-
-          {/* Role Icons */}
-          {roles.map((role) => (
-            <div
-              key={role}
-              className="w-full flex justify-between border-b border-gray-600 py-1 px-5"
-            >
-              <span className="w-[40px] h-[40px] flex items-center justify-center overflow-hidden relative">
-                {RoleIcons[role]}
-              </span>
-              <span className="flex items-center justify-center fake-modal-link">
-                {role}
-              </span>
-            </div>
-          ))}
+      <div
+        className={`${
+          roleDropdownOpen ? "max-h-[500px] border" : "max-h-0 border-0"
+        } bg-slate-800 absolute md:hidden text-center top-[31px] left-0 w-[150px] border-gray-400 shadow-2xl overflow-hidden z-50 transition-all ease-in-out duration-300`}
+      >
+        {/* Faction Icon */}
+        <div className="w-full flex justify-between border border-transparent border-b-gray-600 hover:border-[#ffa500] hover:bg-[#3b444bb9] py-1 px-5">
+          <span className="w-[40px] h-[40px] overflow-hidden relative inline-block">
+            <img
+              loading="lazy"
+              src={`/test_ecgc_2/images/faction/${faction}.png`}
+              alt={`${faction}`}
+              className="absolute top-0 left-0 w-full h-auto"
+            />{" "}
+          </span>
+          <span className="flex items-center justify-center fake-modal-link">
+            {faction}
+          </span>
         </div>
-      )}
+
+        {/* HullType Icon */}
+        <div className="w-full flex justify-between border border-transparent border-b-gray-600 hover:border-[#ffa500] hover:bg-[#3b444bb9] py-1 px-5">
+          <span className="w-[40px] h-[40px] flex items-center justify-center overflow-hidden relative">
+            <img
+              loading="lazy"
+              src={`/test_ecgc_2/images/ship_type/${hullType}.png`}
+              alt={`${hullType}`}
+              className="w-full h-full object-contain"
+            />
+          </span>
+          <span className="flex items-center justify-center fake-modal-link">
+            {hullType}
+          </span>
+        </div>
+
+        {/* Role Icons */}
+        {roles.map((role) => (
+          <div
+            key={role}
+            className="w-full flex justify-between border border-transparent border-b-gray-600 hover:border-[#ffa500] hover:bg-[#3b444bb9] py-1 px-5"
+          >
+            <span className="w-[40px] h-[40px] flex items-center justify-center overflow-hidden relative">
+              {RoleIcons[role]}
+            </span>
+            <span className="flex items-center justify-center fake-modal-link">
+              {role}
+            </span>
+          </div>
+        ))}
+      </div>
 
       {/* Tags (Larger Screen) */}
       <div className="absolute top-0 left-0 transform translate-x-0 hidden md:inline-block">
