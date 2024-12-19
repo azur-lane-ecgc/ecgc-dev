5c4deabf3fd26efff127330380ee99bbd144277e
samheart564 <samheart564@gmail.com>
2024-12-18 16:05:51 -0800

    apparently tailwind first is better? idk why xd

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from "@astrojs/tailwind"
 // https://astro.build/config
 export default defineConfig({
   site: "https://samheart564.github.io/test_ecgc_2/",
-  integrations: [react(), tailwind()],
+  integrations: [tailwind(), react()],
   base: "/test_ecgc_2/",
   output: "static",
   trailingSlash: "ignore",
