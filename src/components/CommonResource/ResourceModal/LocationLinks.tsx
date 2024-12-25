de6ac4f0f634ab28574ef4f3efc27a5405d544d2
samheart564 <samheart564@gmail.com>
2024-12-25 02:45:32 -0800

    added helper component to map locations within table cells

---
+++ src/components/CommonResource/ResourceModal/LocationLinks.tsx
@@ -0,0 +1,52 @@
+interface LocationLinksProps {
+  locations: {
+    name: string
+    wikiLink: string
+    quantity: {
+      amount: number | "RNG"
+      timeFrame:
+        | "one-time"
+        | "daily"
+        | "weekly"
+        | "twice-monthly"
+        | "monthly"
+        | "bimonthly"
+        | null
+    }
+    notes?: string
+  }[]
+}
+
+export const LocationLinks: React.FC<LocationLinksProps> = ({ locations }) => {
+  return (
+    <>
+      {locations &&
+        locations.map((location, index) => (
+          <div key={index} className="text-[13px] leading-normal">
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              href={`https://azurlane.koumakan.jp/wiki/${location.wikiLink.replaceAll(" ", "_")}`}
+            >
+              {location.name}
+            </a>{" "}
+            <span>
+              {location.notes && `(${location.notes})`} -{" "}
+              {location.quantity.amount}{" "}
+              {location.quantity.timeFrame &&
+                location.quantity.timeFrame !== null &&
+                location.quantity.amount !== "RNG" && (
+                  <>
+                    /{" "}
+                    {location.quantity.timeFrame.charAt(0).toUpperCase() +
+                      location.quantity.timeFrame
+                        .slice(1)
+                        .replace("Bimonthly", "2 Months")}
+                  </>
+                )}
+            </span>
+          </div>
+        ))}
+    </>
+  )
+}
