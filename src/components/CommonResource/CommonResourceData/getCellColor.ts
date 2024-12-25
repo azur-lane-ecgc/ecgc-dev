fb02538f092db3f32dc839647a67b9617dbbebe6
samheart564 <samheart564@gmail.com>
2024-12-25 03:49:38 -0800

    added group class to tr

--- src/components/CommonResource/CommonResourceData/getCellColor.ts
+++ src/components/CommonResource/CommonResourceData/getCellColor.ts
@@ -1,18 +1,18 @@
 export const getCellColor = (color: string | undefined | null) => {
-  let colorClass = ""
+  let colorClass = "group "
 
   switch (color) {
     case "red":
-      colorClass = "!bg-[lightcoral] hover:!bg-[#c75e5e]"
+      colorClass += "!bg-[lightcoral] hover:!bg-[#c75e5e]"
       break
     case "green":
-      colorClass = "!bg-[palegreen] hover:!bg-[#52b352]"
+      colorClass += "!bg-[palegreen] hover:!bg-[#52b352]"
       break
     case "sand":
-      colorClass = "!bg-[lemonchiffon] hover:!bg-[rgba(199,193,136,0.9)]"
+      colorClass += "!bg-[lemonchiffon] hover:!bg-[rgba(199,193,136,0.9)]"
       break
     default:
-      colorClass = "!bg-[lightcoral] hover:!bg-[#c75e5e]"
+      colorClass += "!bg-[lightcoral] hover:!bg-[#c75e5e]"
   }
 
   return colorClass
