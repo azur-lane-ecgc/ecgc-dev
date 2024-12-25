bfaa81674bbad25b0292d3793f7bc3a3101a0d48
samheart564 <samheart564@gmail.com>
2024-12-25 03:59:54 -0800

    sorted alphabetically by name

--- dev_tools/_pageInfo.js
+++ dev_tools/_pageInfo.js
@@ -1,27 +1,31 @@
 export const pageInfo = [
   {
-    name: "Changelog2024",
-    path: "src/components/Changelog/Changelog2024Content.astro",
-  },
-  {
     name: "Changelog2023",
     path: "src/components/Changelog/Changelog2023Content.astro",
   },
   {
-    name: "Newbie Tips",
-    path: "src/components/NewbieTips/NewbieTipsContent.astro",
-  },
-  {
-    name: "Equipment",
-    path: "src/components/Equipment/EquipmentContent.astro",
+    name: "Changelog2024",
+    path: "src/components/Changelog/Changelog2024Content.astro",
   },
   {
     name: "Early Ship Recommendations",
     path: "src/components/EarlyShipRecs/EarlyShipRecsContent.astro",
   },
   {
+    name: "Equipment",
+    path: "src/components/Equipment/EquipmentContent.astro",
+  },
+  {
     name: "Farming Guide",
     path: "src/components/Farming/FarmingGuideContent.astro",
+  },
+  {
+    name: "Fleet Technology Guide",
+    path: "src/components/FleetTechnology/FleetTechnologyContent.astro",
+  },
+  {
+    name: "Newbie Tips",
+    path: "src/components/NewbieTips/NewbieTipsContent.astro",
   },
   {
     name: "Research Guide",
@@ -30,9 +34,5 @@ export const pageInfo = [
   {
     name: "Shop Priority Guide",
     path: "src/components/ShopPriority/ShopPriorityContent.astro",
-  },
-  {
-    name: "Fleet Technology Guide",
-    path: "src/components/FleetTechnology/FleetTechnologyContent.astro",
   },
 ]
