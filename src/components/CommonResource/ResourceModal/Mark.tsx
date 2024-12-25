d5857180d93b387a48fc8a86d7d6b9904a32f7ed
samheart564 <samheart564@gmail.com>
2024-12-25 03:50:13 -0800

    fixed unicode mark characters + classes

--- src/components/CommonResource/ResourceModal/Mark.tsx
+++ src/components/CommonResource/ResourceModal/Mark.tsx
@@ -5,8 +5,10 @@ interface MarkProps {
 
 export const Mark: React.FC<MarkProps> = ({ mark, className = "" }) => {
   return (
-    <div className={`${className}`}>
-      {mark === "check" ? "\u2714" : "\u274C"}
+    <div
+      className={`${className} !text-4xl font-bold text-black block ${mark && "group-hover:hidden"}`}
+    >
+      {mark === "check" ? "\u2713" : "\u2717"}
     </div>
   )
 }
