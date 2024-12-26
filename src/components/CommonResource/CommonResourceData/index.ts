3052a49641d801e99ac04dff3639997efd419407
samheart564 <samheart564@gmail.com>
2024-12-25 21:49:07 -0800

    added coredata

--- src/components/CommonResource/CommonResourceData/index.ts
+++ src/components/CommonResource/CommonResourceData/index.ts
@@ -2,5 +2,6 @@ import type { ResourceProps } from "./types"
 
 import CoinData from "./coin"
 import OilData from "./oil"
+import CoreData from "./coreData"
 
-export const ResourceData: ResourceProps[] = [CoinData, OilData]
+export const ResourceData: ResourceProps[] = [CoinData, OilData, CoreData]
