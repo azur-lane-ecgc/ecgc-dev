bcf62c227351f63bafabc0e65d1914adff2424b4
samheart564 <samheart564@gmail.com>
2024-12-25 18:44:59 -0800

    instead of color, just use a new symbol if optimal

--- src/components/CommonResource/ResourceModal/Mark.tsx
+++ src/components/CommonResource/ResourceModal/Mark.tsx
@@ -13,8 +13,8 @@ export const Mark: React.FC<MarkProps> = ({ mark, className = "" }) => (
       !!mark
         ? "absolute top-0 left-1 !text-base sm:text-lg md:!text-xl"
         : "!text-3xl block"
-    } font-bold ${mark?.optimal ? "!text-emerald-700" : "!text-black"}`}
+    } font-bold !text-black`}
   >
-    {mark?.mark === "check" ? "\u2713" : "\u2717"}
+    {mark?.optimal ? "\u2055" : mark?.mark === "check" ? "\u2713" : "\u2717"}
   </div>
 )
