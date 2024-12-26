1fa7c163e746c4a1dc1ee57c5c2b7ba8d9765e9b
samheart564 <samheart564@gmail.com>
2024-12-25 20:52:47 -0800

    if locations doesn't exist just return nothing, not empty div

--- src/components/CommonResource/ResourceModal/LocationLinks.tsx
+++ src/components/CommonResource/ResourceModal/LocationLinks.tsx
@@ -23,38 +23,40 @@ export const LocationLinks: React.FC<LocationLinksProps> = ({
   locations,
   className = "",
 }) => {
+  if (!!!locations) {
+    return false
+  }
   return (
     <div className={`${className}`}>
-      {locations &&
-        locations.map((location, index) => (
-          <div key={index} className="text-[12px] leading-normal">
-            <a
-              rel="noopener noreferrer"
-              target="_blank"
-              href={`https://azurlane.koumakan.jp/wiki/${location.wikiLink.replaceAll(" ", "_")}`}
-              className="font-bold !text-[#0047ab] bg-transparent no-underline hover:!text-[#3c5679] hover:underline active:!text-[#3c5679] active:underline"
-            >
-              {location.name}
-            </a>{" "}
-            <span className="text-black font-semibold">
-              {location.notes && `(${location.notes})`}
-              {location.quantity.amount && ` - ${location.quantity.amount}`}
-              {location.quantity.timeFrame &&
-                location.quantity.amount !== "RNG" && (
-                  <>
-                    {" "}
-                    /{" "}
-                    {location.quantity.timeFrame
-                      .replace("daily", "Day")
-                      .replace("weekly", "Week")
-                      .replace("bimonthly", "2 Months")
-                      .replace("monthly", "Month")
-                      .replace("cycle", "Cycle")}
-                  </>
-                )}
-            </span>
-          </div>
-        ))}
+      {locations.map((location, index) => (
+        <div key={index} className="text-[12px] leading-normal">
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href={`https://azurlane.koumakan.jp/wiki/${location.wikiLink.replaceAll(" ", "_")}`}
+            className="font-bold !text-[#0047ab] bg-transparent no-underline hover:!text-[#3c5679] hover:underline active:!text-[#3c5679] active:underline"
+          >
+            {location.name}
+          </a>{" "}
+          <span className="text-black font-semibold">
+            {location.notes && `(${location.notes})`}
+            {location.quantity.amount && ` - ${location.quantity.amount}`}
+            {location.quantity.timeFrame &&
+              location.quantity.amount !== "RNG" && (
+                <>
+                  {" "}
+                  /{" "}
+                  {location.quantity.timeFrame
+                    .replace("daily", "Day")
+                    .replace("weekly", "Week")
+                    .replace("bimonthly", "2 Months")
+                    .replace("monthly", "Month")
+                    .replace("cycle", "Cycle")}
+                </>
+              )}
+          </span>
+        </div>
+      ))}
     </div>
   )
 }
