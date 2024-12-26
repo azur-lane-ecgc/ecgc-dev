5e25cf41f06211af083e34a3c02e207c22cd59fb
samheart564 <samheart564@gmail.com>
2024-12-25 22:42:56 -0800

    added crystal fragments

--- src/components/CommonResource/CommonResourceData/index.ts
+++ src/components/CommonResource/CommonResourceData/index.ts
@@ -2,5 +2,6 @@ import CoinData from "./coin"
 import OilData from "./oil"
 import CoreData from "./coreData"
 import GuildTokenData from "./guildToken"
+import CrystalFragmentData from "./crystalFragment"
 
-export { CoinData, OilData, CoreData, GuildTokenData }
+export { CoinData, OilData, CoreData, GuildTokenData, CrystalFragmentData }
