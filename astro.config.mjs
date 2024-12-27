ca654bb5f1a374d70a5658ddd0ba8c0dc9ad0e0d
samheart564 <samheart564@gmail.com>
2024-12-26 20:44:07 -0800

    configured compress to only act on images and svg

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,15 @@ import tailwind from "@astrojs/tailwind"
 // https://astro.build/config
 export default defineConfig({
   site: "https://samheart564.github.io/test_ecgc_2/",
-  integrations: [tailwind(), react()],
+  integrations: [
+    tailwind(),
+    react(),
+    (await import("@playform/compress")).default({
+			CSS: false,
+			HTML: false,
+			JavaScript: false,
+		}),
+  ],
   base: "/test_ecgc_2/",
   output: "static",
   trailingSlash: "ignore",
