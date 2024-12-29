eca866ce7d0141e1dea44a06ac4a09d90b0cbc80
samheart564 <samheart564@gmail.com>
2024-12-28 22:07:21 -0800

    added tools page (for now, probably will add more later)

---
+++ src/components/Tools/ToolTableFilters.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react"
+
+import { ComboBox } from "@components/_common/ComboBox"
+
+import { ToolTable } from "./ToolTable"
+
+const categoryDisplayMapping: { [key: string]: string } = {
+  Informational: "info",
+  Fun: "fun",
+  Data: "data",
+}
+
+const displayCategories = Object.keys(categoryDisplayMapping) as Array<string>
+
+export const ToolTableFilters = () => {
+  const [category, setCategory] = useState<string | null>(null)
+
+  const handleSelect = (selected: string | null) => {
+    const mappedCategory = selected ? categoryDisplayMapping[selected] : null
+    setCategory(mappedCategory)
+  }
+
+  return (
+    <div className="flex flex-col gap-4">
+      <ComboBox
+        title="Category"
+        options={displayCategories}
+        onSelect={handleSelect}
+      />
+      <ToolTable category={category} />
+    </div>
+  )
+}
