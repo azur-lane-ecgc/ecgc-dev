3cde176c40f051a1b046cbe45998590665e04779
samheart564 <samheart564@gmail.com>
2025-01-06 13:26:44 -0800

    added sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,15 @@ import { defineConfig } from "astro/config"
 import react from "@astrojs/react"
 import tailwind from "@astrojs/tailwind"
 
+import sitemap from "@astrojs/sitemap"
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://samheart564.github.io/test_ecgc_2/",
   integrations: [
     tailwind(),
     react(),
+    sitemap(),
     (await import("@playform/compress")).default(),
   ],
   base: "/test_ecgc_2/",
