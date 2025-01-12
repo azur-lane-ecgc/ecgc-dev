817aeef94501374bc936766de81e0bf70970d18f
samheart564 <samheart564@gmail.com>
2025-01-11 21:20:55 -0800

    fixed truncate string function

--- src/utils/truncateString/truncateString.ts
+++ src/utils/truncateString/truncateString.ts
@@ -1,7 +1,7 @@
 export const truncateString = (str: string, maxLen: number): string => {
-  if (str.length < maxLen) {
+  if (str.length <= maxLen) {
     return str
   }
 
-  return str.slice(0, str.length - (maxLen + 4)) + " ..."
+  return str.slice(0, maxLen - 3) + "..."
 }
