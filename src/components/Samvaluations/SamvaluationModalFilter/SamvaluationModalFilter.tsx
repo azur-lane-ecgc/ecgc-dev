990aa2e1cc161cc1bb84504b2ed853ef96550325
samheart564 <samheart564@gmail.com>
2025-01-11 22:03:02 -0800

    moved modal group to its own component

--- src/components/Samvaluations/SamvaluationModalFilter/SamvaluationModalFilter.tsx
+++ src/components/Samvaluations/SamvaluationModalFilter/SamvaluationModalFilter.tsx
@@ -1,3 +1,30 @@
+import { ItemContainer } from "@components/_common/ItemCell"
+import { ShipModal } from "@components/_common/ShipModal"
+
+import type { ShipData } from "@data/types/ships"
+import shipData from "@data/data/ships.json"
+const ships = shipData as Record<number, ShipData>
+
 export const SamvaluationModalFilter = () => {
-  return false
+  return (
+    <>
+      <ItemContainer>
+        {Object.keys(ships).map((key) => {
+          const ship = ships[parseInt(key, 10)]
+          if (!ship) return false
+          return (
+            <ShipModal
+              id={ship.id}
+              trigger={{
+                iconNote: null,
+                descriptionNote: null,
+                largeDescNote: false,
+                hasBorder: true,
+              }}
+            />
+          )
+        })}
+      </ItemContainer>
+    </>
+  )
 }
