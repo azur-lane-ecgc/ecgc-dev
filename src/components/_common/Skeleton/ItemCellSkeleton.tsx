1773f8a9b0a6292447de7861eceaa6d897d38949
samheart564 <samheart564@gmail.com>
2025-01-12 16:59:55 -0800

    typo fix

--- src/components/_common/Skeleton/ItemCellSkeleton.tsx
+++ src/components/_common/Skeleton/ItemCellSkeleton.tsx
@@ -7,7 +7,7 @@ export const ItemCellSkeleton = () => {
       className={`${"border modifiedShipRowCell text-center"} border-gray-400`}
     >
       <div className="relative">
-        {/* IMG Skeleton HEre */}
+        {/* IMG Skeleton HERE */}
         <Skeleton className="overflow-hidden my-1.5 mx-auto w-[60px] p-0.5 shadow-[0_10px_25px_0_rgb(0,0,0)] h-[60px] mb-2" />
         {/* TEXT SKELETON HERE */}
         <div className="flex flex-col items-center justify-center">
