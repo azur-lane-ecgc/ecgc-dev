8fe49805e32716aba4844521facaea6c0d0e9590
samheart564 <samheart564@gmail.com>
2025-01-16 14:46:44 -0800

    new event updates

--- src/components/_common/Constants/lastUpdated.ts
+++ src/components/_common/Constants/lastUpdated.ts
@@ -1,2 +1,2 @@
-export const endGameRankingsUpdateDate = "01/11/2025"
-export const ehpUpdateDate = "12/25/2025"
+export const endGameRankingsUpdateDate = "01/15/2025"
+export const ehpUpdateDate = "01/16/2025"
