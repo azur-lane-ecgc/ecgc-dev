aabfe64a808666932f6ec09db680c04671cbf64c
samheart564 <samheart564@gmail.com>
2025-01-17 20:47:30 -0800

    more script stuff yay

--- dev_tools/final.sh
+++ dev_tools/final.sh
@@ -2,11 +2,11 @@
 
 cd dev_tools || { echo "dev_tools directory not found!"; exit 1; }
 
+deactivate
+
 if [ -d "ENV" ]; then
     rm -rf ENV
     echo "ENV folder deleted."
 else
     echo "ENV folder not found, nothing to delete."
 fi
-
-echo "Deleted temp folders..."
