bb73fea861950000f1d704e74cfc3d0ea9258730
samheart564 <samheart564@gmail.com>
2025-02-17 15:19:18 -0800

    tailwind v4 update

--- astro.config.ts
+++ astro.config.ts
@@ -2,26 +2,24 @@
 import { defineConfig } from "astro/config"
 
 import react from "@astrojs/react"
-import tailwind from "@astrojs/tailwind"
-
 import sitemap from "@astrojs/sitemap"
+import tailwindcss from "@tailwindcss/vite"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://samheart564.github.io/test_ecgc_2/",
   integrations: [
-    tailwind(),
     react(),
     sitemap(),
-    (await import("@playform/compress")).default({
-      CSS: false,
-      HTML: false,
-      JavaScript: false,
-      Exclude: [
-        (File: string) =>
-          File.includes("SiteIcon"),
-      ],
-    }),
+    // (await import("@playform/compress")).default({
+    //   CSS: false,
+    //   HTML: false,
+    //   JavaScript: false,
+    //   Exclude: [
+    //     (File: string) =>
+    //       File.includes("SiteIcon"),
+    //   ],
+    // }),
   ],
   base: "/test_ecgc_2/",
   output: "static",
@@ -32,6 +30,7 @@ export default defineConfig({
       stringify: true,
     },
     plugins: [
+      tailwindcss(),
       react({
         babel: {
           plugins: [
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,51 +1,7 @@
 import type { Config } from "tailwindcss"
-import type { PluginAPI } from "tailwindcss/types/config"
 
 const tailwindConfig: Config = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      screens: {
-        sm: "576px",
-        md: "768px",
-        lg: "992px",
-        xl: "1200px",
-        "2xl": "1400px",
-      },
-      colors: {
-        "ecgc-primary": "#121212",
-        "ecgc-secondary": "#1b1b1b",
-      },
-    },
-  },
-  corePlugins: {
-    container: false,
-  },
-  plugins: [
-    function ({ addComponents }: PluginAPI) {
-      addComponents({
-        ".container": {
-          width: "100%",
-          maxWidth: "100%",
-          "@screen sm": {
-            maxWidth: "540px",
-          },
-          "@screen md": {
-            maxWidth: "720px",
-          },
-          "@screen lg": {
-            maxWidth: "960px",
-          },
-          "@screen xl": {
-            maxWidth: "1140px",
-          },
-          "@screen 2xl": {
-            maxWidth: "1320px",
-          },
-        },
-      })
-    },
-  ],
 }
 
 export default tailwindConfig
