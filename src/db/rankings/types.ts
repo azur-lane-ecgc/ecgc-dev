a88ccdb77b4d94e2cfe11f91a1bc8782a907490f
samheart564 <samheart564@gmail.com>
2025-02-22 19:07:22 -0800

    inheritance instead of 3 separate ones xd

--- src/db/rankings/types.ts
+++ src/db/rankings/types.ts
@@ -2,14 +2,29 @@ export interface ShipRankingTypes {
   ship: string
 }
 
-export interface MainFleetRankingProps {
+export interface BaseFleetRankingProps {
   notes?: string | null
   nameNote?: string | null
 
   // Rank
   hardarbiter: string
-  meta: string
   cm: string
+
+  // Usage
+  consistency?: number | null
+  fleetreq?: number | null
+  gearreq?: number | null
+
+  // Offense
+  lightdmg: number
+  mediumdmg: number
+  heavydmg: number
+  offensivebuff?: number | null
+}
+
+export interface MainFleetRankingProps extends BaseFleetRankingProps {
+  // Rank
+  meta: string
   w14mob: string
   w14boss: string
   w15mob: string
@@ -17,18 +32,11 @@ export interface MainFleetRankingProps {
   ex: string
 
   // Usage
-  consistency?: number | null
-  fleetreq?: number | null
-  gearreq?: number | null
   flagreq?: number | null
 
   // Offense
-  lightdmg: number
-  mediumdmg: number
-  heavydmg: number
   aoedmg: number
   dmguptime?: number | null
-  offensivebuff?: number | null
 
   // Defense
   selfsurvival?: number | null
@@ -38,31 +46,17 @@ export interface MainFleetRankingProps {
   vgsurvival?: number | null
 }
 
-export interface VanguardFleetRankingProps {
-  notes?: string | null
-  nameNote?: string | null
-
+export interface VanguardFleetRankingProps extends BaseFleetRankingProps {
   // Rank
-  hardarbiter: string
   meta: string
-  cm: string
   w14mob: string
   w14boss: string
   w15mob: string
   w15boss: string
   ex: string
 
-  // Usage
-  consistency?: number | null
-  fleetreq?: number | null
-  gearreq?: number | null
-
   // Offense
-  lightdmg: number
-  mediumdmg: number
-  heavydmg: number
   aoedmg: number
-  offensivebuff?: number | null
 
   // Defense
   selfsurvival?: number | null
@@ -71,23 +65,10 @@ export interface VanguardFleetRankingProps {
   defensivebuff?: number | null
 }
 
-export interface SSFleetRankingProps {
-  notes?: string | null
-  nameNote?: string | null
-
+export interface SSFleetRankingProps extends BaseFleetRankingProps {
   // Rank
-  hardarbiter: string
-  cm: string
   campaign?: string | null
 
   // Usage
-  consistency?: number | null
-  fleetreq?: number | null
   flagreq?: number | null
-
-  // Offense
-  lightdmg: number
-  mediumdmg: number
-  heavydmg: number
-  offensivebuff?: number | null
 }
