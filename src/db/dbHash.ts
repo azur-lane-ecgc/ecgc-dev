c03223efd3e57550675b76cf06974d9eb9ff0122
samheart564 <samheart564@gmail.com>
2025-02-28 16:22:53 -0800

    fixed hashing function

--- src/db/dbHash.ts
+++ src/db/dbHash.ts
@@ -1,3 +1,10 @@
 export const generateHash = (data: any): string => {
-  return Bun.hash(JSON.stringify(data)).toString(16)
+  const str = JSON.stringify(data)
+  let hash = 0
+
+  for (let i = 0; i < str.length; i++) {
+    hash = (hash * 31 + str.charCodeAt(i)) | 0
+  }
+
+  return hash.toString(36)
 }
