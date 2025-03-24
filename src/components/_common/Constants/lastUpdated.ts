2993ba4481157cfefea286882bfea2ac10cf24c1
samheart564 <samheart564@gmail.com>
2025-03-24 13:20:34 -0700

    devtool updates

--- src/components/_common/Constants/lastUpdated.ts
+++ src/components/_common/Constants/lastUpdated.ts
@@ -1,2 +1,2 @@
-export const endGameRankingsUpdateDate = "03/13/2025"
-export const ehpUpdateDate = "02/27/2025"
+export const endGameRankingsUpdateDate = "03/21/2025"
+export const ehpUpdateDate = "03/23/2025"
