abc385fc7957944370c6124baf03883c0f1e0bab
samheart564 <samheart564@gmail.com>
2025-04-16 21:34:33 -0700

    devtool + dependency update

---
+++ dev_tools/ships/shipRoleParse/data/vgSupport.ts
@@ -0,0 +1,14 @@
+export const vgDMGSupport = new Set<string>([
+  "Aurora",
+  "Gloucester",
+  "Helena",
+  "Sendai",
+  "Alfredo Oriani",
+  "Avrora",
+  "Chapayev",
+  "Emden",
+  "Princess Hime",
+  "Kashino",
+  "Ting An",
+  "Swiftsure"
+])
