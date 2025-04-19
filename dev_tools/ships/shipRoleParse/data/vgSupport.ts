36ae9ccb2f49784d1224320f4516a24a48f3c461
samheart564 <samheart564@gmail.com>
2025-04-19 13:41:35 -0700

    initial vg support list

--- dev_tools/ships/shipRoleParse/data/vgSupport.ts
+++ dev_tools/ships/shipRoleParse/data/vgSupport.ts
@@ -1,14 +1,56 @@
-export const vgDMGSupport = new Set<string>([
+// <fleet of ship> | <buff receiver> | <buff type> | "Support"
+
+export const vgAllDMGSupport = new Set<string>([
   "Aurora",
   "Gloucester",
   "Helena",
+  "Le Mars",
+  "Cleveland",
   "Sendai",
-  "Alfredo Oriani",
+  "Santa Fe",
+])
+
+export const vgAllSlowSupport = new Set<string>([
+  "Ägir",
+  "Eldridge",
+  "Kirov META",
+  "Northampton II",
+  "Admiral Hipper META",
+])
+
+export const vgBBSupport = new Set<string>(["Plymouth"])
+
+export const vgCVSupport = new Set<string>([
+  "Sirius",
+  "Baltimore",
+  "Bellona",
+  "Hammann II",
+  "Kazagumo",
+  "Stephen Potter",
+  "Scylla",
+])
+
+export const vgVgDMGSupport = new Set<string>([
   "Avrora",
   "Chapayev",
   "Emden",
-  "Princess Hime",
-  "Kashino",
-  "Ting An",
-  "Swiftsure"
+  "Fubuki",
+])
+
+export const vgVgTRPSupport = new Set<string>([
+  "Noshiro μ",
+  "Sendai",
+  "Tanikaze",
+  "Jintsuu",
+  "Jintsuu META",
+  "Naganami",
+  "Noshiro",
+])
+
+export const vgVgSustainSupport = new Set<string>([
+  "Eldridge",
+  "Jeanne d'Arc",
+  "Anchorage",
+  "Harbin",
+  "San Francisco",
 ])
