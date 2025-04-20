e5159051fb5aab2ae22570706ca842e7f58d9d6b
samheart564 <samheart564@gmail.com>
2025-04-19 23:47:02 -0700

    another draft yay

---
+++ dev_tools/ships/shipRoleParse/data/vgSupport2.ts
@@ -0,0 +1,48 @@
+// All VANGUARD FLEET ships that offer relevant support
+
+/*
+  Naming Convention:
+  <fleet of ship> | <buff receiver> | <buff type> | "Support"
+*/
+
+export const vgAllDmgSupport = new Set<string>([
+  "Aurora",
+  "Gloucester",
+  "Helena",
+])
+
+export const vgAllFlareSupport = new Set<string>([
+  "Alfredo Oriani",
+  "Sendai",
+  "Santa Fe",
+])
+
+export const vgAllSlowBlackHoleSupport = new Set<string>([
+  "<FILLER>",
+  "<FILLER>",
+  "<FILLER>",
+])
+
+export const vgBBDmgSupport = new Set<string>(["Plymouth"])
+
+export const vgCVSupport = new Set<string>([
+  "Sirius",
+  "Bellona",
+  "Hammann II",
+  "Kazagumo",
+  "Scylla",
+])
+
+export const vgVgDmgSupport = new Set<string>([
+  "Fubuki",
+  "<FILLER>",
+  "<FILLER>",
+])
+
+export const vgVgTrpSupport = new Set<string>([
+  "Noshiro μ",
+  "Jintsuu",
+  "Jintsuu META",
+  "Noshiro",
+  "Tanikaze",
+])
