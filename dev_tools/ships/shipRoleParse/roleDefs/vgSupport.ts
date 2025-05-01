f69b6f2bde9a2c17e60b0217597a66b8a785e04e
samheart564 <samheart564@gmail.com>
2025-05-01 14:03:03 -0700

    moved folder

---
+++ dev_tools/ships/shipRoleParse/roleDefs/vgSupport.ts
@@ -0,0 +1,30 @@
+// All support ships with correct categories and naming convention
+/*
+  Naming Convention:
+  <fleet of ship> | <buff receiver> | <buff type> | "Support"
+*/
+
+export const vgMainDmgSupport = new Set(["Plymouth"])
+
+export const vgCVDmgSupport = new Set(["Kazagumo", "Sirius"])
+
+export const vgAllDMGSupport = new Set(["Helena", "Aurora", "Z52"])
+
+export const vgAllSlowSupport = new Set(["Eldridge", "Ägir", "Mogador"])
+
+export const vgVgTRPSupport = new Set([
+  "Noshiro",
+  "Jintsuu",
+  "Jintsuu META",
+  "Noshiro μ",
+  "Tanikaze",
+  "Fubuki",
+])
+
+export const vgVgBulkSupport = new Set([
+  "Eldridge",
+  "Jeanne d'Arc",
+  "Anchorage",
+  "Jintsuu META",
+  "San Francisco",
+])
