d345cce64fe008febf4624ded8a7c3dacdab2e3e
samheart564 <samheart564@gmail.com>
2025-05-29 13:13:24 -0700

    dependency and submodule update

--- src/components/_common/Constants/lastUpdated.ts
+++ src/components/_common/Constants/lastUpdated.ts
@@ -1,2 +1,2 @@
-export const endGameRankingsUpdateDate = "05/22/2025"
-export const ehpUpdateDate = "04/23/2025"
+export const endGameRankingsUpdateDate = "05/25/2025"
+export const ehpUpdateDate = "05/28/2025"
