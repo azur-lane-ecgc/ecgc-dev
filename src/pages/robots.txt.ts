b4e7ed7866ac9bc0374bcf326c58923a8bf2c444
samheart564 <samheart564@gmail.com>
2025-06-11 18:20:31 -0700

    updated robots.txt file

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,13 +1,38 @@
 // @ts-nocheck
 import type { APIRoute } from "astro"
 
-const getRobotsTxt = (sitemapURL: URL) => `
-User-agent: *
-Disallow: /
-`
-// Sitemap: ${sitemapURL.href}
+const GOOGLE_BOTS = [
+  "Googlebot",
+  "Googlebot-Image",
+  "Googlebot-News",
+  "Googlebot-Video",
+  "Mediapartners-Google",
+  "AdsBot-Google",
+  "AdsBot-Google-Mobile",
+  "APIs-Google",
+  "Google-Read-Aloud",
+  "DuplexWeb-Google",
+  "FeedFetcher-Google",
+]
+
+const getRobotsTxt = (sitemapURL: URL) => {
+  let rules = ""
+
+  for (const bot of GOOGLE_BOTS) {
+    rules += `User-agent: ${bot}\nDisallow:\n\n`
+  }
+
+  rules += `User-agent: *\nDisallow: /\n\n`
+  rules += `Sitemap: ${sitemapURL.href}\n`
+
+  return rules
+}
 
 export const GET: APIRoute = ({ site }) => {
   const sitemapURL = new URL("sitemap-index.xml", site)
-  return new Response(getRobotsTxt(sitemapURL))
+  return new Response(getRobotsTxt(sitemapURL), {
+    headers: {
+      "Content-Type": "text/plain",
+    },
+  })
 }
