724f4d500cf8db19808fe6805d21c2db4a4c0cb9
samheart564 <samheart564@gmail.com>
2025-06-11 18:10:21 -0700

    separated gsheets scraping & imgur uploading

--- dev_tools/python/gsheets2imgur.sh
+++ dev_tools/python/gsheets2imgur.sh
@@ -1,3 +1,21 @@
+
 source dev_tools/python/install.sh
 
-uv run automated_imgur_upload/imgur.py
+run_gsheets2img() {
+    uv run automated_imgur_upload/gsheets2img.py
+}
+
+run_imgur() {
+    uv run automated_imgur_upload/imgur.py
+}
+
+if [[ "$1" == "-g" ]]; then
+    run_gsheets2img
+    exit 0
+elif [[ "$1" == "-i" ]]; then
+    run_imgur
+    exit 0
+else
+    run_gsheets2img
+    run_imgur
+fi
