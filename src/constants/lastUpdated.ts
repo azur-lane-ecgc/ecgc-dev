c9986b1d95e127913e52bc7c61b84369cad17981
therealsamyak <therealsamyak@gmail.com>
2025-09-02 18:52:01 -0700

    package & devtool updates

--- src/constants/lastUpdated.ts
+++ src/constants/lastUpdated.ts
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 export const endGameRankingsUpdateDate = "09/05/2025"
+=======
+export const endGameRankingsUpdateDate = "09/01/2025"
+>>>>>>> 843e920 (package & devtool updates)
 export const ehpUpdateDate = "07/24/2025"
